993e242e7c5b122d9dd71bbecd8bd009f3f5db8c
aaronshin43 <aaronshin.exe@gmail.com>
2025-12-02 15:46:26 -0500

    day2 part1

--- day2.py
+++ day2.py
@@ -1,12 +1,29 @@
 # https://adventofcode.com/2025/day/2
 ID = input().split(",")
 #print(ID)
+result = 0
 while ID:
-    current = ID.pop()
-    firstID = int(current[:current.index('-')])
+    currentRange = ID.pop()
+    firstID = int(currentRange[:currentRange.index('-')])
     #print(firstID)
-    lastID = int(current[current.index('-')+1:])
+    lastID = int(currentRange[currentRange.index('-')+1:])
     #print(lastID)
     for i in range(firstID, lastID+1):
         #print(i)
-        continue
+        currentID = str(i)
+        if len(currentID) % 2 != 0:
+            continue
+
+        if currentID[0] == '0':
+            result += i
+            continue
+        
+        firstHalf = currentID[:len(currentID)//2]
+        #print(firstHalf)
+        secondHalf = currentID[len(currentID)//2:]
+        #print(secondHalf)
+        if firstHalf == secondHalf:
+            #print(f"INVALID ID: {currentID}")
+            result += i
+
+print(result)
