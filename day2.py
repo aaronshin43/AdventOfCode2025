4e4642b9d919b05a46ea2f200894401bb1aa4b76
aaronshin43 <aaronshin.exe@gmail.com>
2025-12-02 16:09:58 -0500

    day2 part2

--- day2.py
+++ day2.py
@@ -1,4 +1,6 @@
 # https://adventofcode.com/2025/day/2
+import math
+
 ID = input().split(",")
 #print(ID)
 result = 0
@@ -11,19 +13,40 @@ while ID:
     for i in range(firstID, lastID+1):
         #print(i)
         currentID = str(i)
-        if len(currentID) % 2 != 0:
-            continue
-
+        # if len(currentID) % 2 != 0:
+        #     continue
+        
         if currentID[0] == '0':
+            print(f"INVALID ID: {currentID}")
             result += i
             continue
         
-        firstHalf = currentID[:len(currentID)//2]
-        #print(firstHalf)
-        secondHalf = currentID[len(currentID)//2:]
-        #print(secondHalf)
-        if firstHalf == secondHalf:
-            #print(f"INVALID ID: {currentID}")
-            result += i
+        # firstHalf = currentID[:len(currentID)//2]
+        # #print(firstHalf)
+        # secondHalf = currentID[len(currentID)//2:]
+        # #print(secondHalf)
+        # if firstHalf == secondHalf:
+        #     #print(f"INVALID ID: {currentID}")
+        #     result += i
+        
+        divisors = [j for j in range(1, len(currentID) + 1) if len(currentID) % j == 0]
+        #print(divisors)
+        divisors.pop()
+        while divisors:
+            currentDivisor = divisors.pop()
+            recursiveTerm = currentID[:currentDivisor]
+            #print(recursiveTerm)
+            Invalid = True
+            for j in range(len(currentID)):
+                if recursiveTerm[j%len(recursiveTerm)] != currentID[j]:
+                    Invalid = False
+            if Invalid:
+                #print(f"INVALID ID: {currentID}")
+                result += i
+                divisors = []
+        #print("=======================================")
+
 
 print(result)
+
+#11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
