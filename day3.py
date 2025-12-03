15107ed40ac1f5045971449d22b6068b8ffbe80c
aaronshin43 <aaronshin.exe@gmail.com>
2025-12-03 11:59:54 -0500

    day3 part2

--- day3.py
+++ day3.py
@@ -1,3 +1,5 @@
+#import math
+
 banks = []
 while True:
     bank = input()
@@ -6,21 +8,39 @@ while True:
     banks.append(bank)
 #print(banks)
 
+# total = 0
+# while banks:
+#     currentBank: str = banks.pop()
+#     firstDigit = 0
+#     for i in range(len(currentBank)-1):
+#         if int(currentBank[i]) > firstDigit:
+#             firstDigit = int(currentBank[i])
+#     #print(firstDigit)
+#     secondDigit = 0
+#     for j in currentBank[currentBank.index(str(firstDigit))+1:]:
+#         if int(j) > secondDigit:
+#             secondDigit = int(j)
+#     #print(secondDigit)
+#     #print((10*firstDigit) + secondDigit)
+#     #print('-------------------')
+#     total += (10*firstDigit) + secondDigit
+
+# print(total)
+
 total = 0
 while banks:
+    #subtotal = 0
     currentBank: str = banks.pop()
-    firstDigit = 0
-    for i in range(len(currentBank)-1):
-        if int(currentBank[i]) > firstDigit:
-            firstDigit = int(currentBank[i])
-    #print(firstDigit)
-    secondDigit = 0
-    for j in currentBank[currentBank.index(str(firstDigit))+1:]:
-        if int(j) > secondDigit:
-            secondDigit = int(j)
-    #print(secondDigit)
-    #print((10*firstDigit) + secondDigit)
-    #print('-------------------')
-    total += (10*firstDigit) + secondDigit
+    startIndex = 0
+    for i in range(11, -1, -1):
+        compareDigit = 0
+        #print(currentBank[startIndex:len(currentBank)-i])
+        for j in range(startIndex, len(currentBank)-i):
+            if int(currentBank[j]) > compareDigit:
+                compareDigit = int(currentBank[j])
+                startIndex = j+1
+        #subtotal += compareDigit*pow(10,i)
+        total += compareDigit*pow(10,i)
+    #print(subtotal)
 
 print(total)
