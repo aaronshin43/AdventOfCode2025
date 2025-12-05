f27462de6a636065a0170d4d44be563b1df5ef11
aaronshin43 <aaronshin.exe@gmail.com>
2025-12-04 21:27:58 -0500

    day4 part2

--- day4.py
+++ day4.py
@@ -1,3 +1,5 @@
+# https://adventofcode.com/2025/day/4
+
 rolls = []
 while True:
     roll = list(input())
@@ -14,29 +16,46 @@ moveY = [-1, -1, -1, 0, 0, 1, 1, 1]
 6 7 8
 '''
 
-count = 0
-for i in range(len(rolls)):
-    for j in range(len(rolls[i])):
-        # print(i, j)
-        neighbors = 8
-        for k in range(8):
-            if rolls[i][j] == "@":
-                neighborX, neighborY = j+moveX[k], i+moveY[k]
-                # print(neighborX, neighborY)
-                if neighborX < 0 or neighborX >= len(rolls[i]) or neighborY < 0 or neighborY >= len(rolls):
-                    neighbors -= 1
-                    # print("neighbor-1")
-                elif rolls[neighborY][neighborX] == ".":
-                    neighbors -= 1
-                    # print("neighbor-1")
-            else:
-                # print("Not @")
-                break
-        # print(f"neighbors: {neighbors}")
-        if neighbors < 4:
-            count += 1
-    #     print(f"Current: {count}")
-    #     print("---------------")
-    # print("====================")
+def findRemovable(rolls):
+    count = 0
+    removable = []
+    for i in range(len(rolls)):
+        for j in range(len(rolls[i])):
+            # print(i, j)
+            neighbors = 8
+            for k in range(8):
+                if rolls[i][j] == "@":
+                    neighborX, neighborY = j+moveX[k], i+moveY[k]
+                    # print(neighborX, neighborY)
+                    if neighborX < 0 or neighborX >= len(rolls[i]) or neighborY < 0 or neighborY >= len(rolls):
+                        neighbors -= 1
+                        # print("neighbor-1")
+                    elif rolls[neighborY][neighborX] == ".":
+                        neighbors -= 1
+                        # print("neighbor-1")
+                else:
+                    # print("Not @")
+                    break
+            # print(f"neighbors: {neighbors}")
+            if neighbors < 4:
+                count += 1
+                removable.append((i, j))
+        #     print(f"Current: {count}")
+        #     print("---------------")
+        # print("====================")
+    return count, removable
 
-print(count)
+removed = 0
+while True:
+    count, removable = findRemovable(rolls)
+    if count == 0:
+        break
+    for i in removable:
+        rolls[i[0]][i[1]] = '.'
+        removed += 1
+
+# for i in range(len(rolls)):
+#     print(rolls[i])
+
+print(removed)
+    
