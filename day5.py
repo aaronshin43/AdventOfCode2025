ff173b0f0758ada381057647ac204bdf3fd09724
aaronshin43 <aaronshin.exe@gmail.com>
2025-12-05 17:20:29 -0500

    day5 part2

--- day5.py
+++ day5.py
@@ -36,11 +36,58 @@ for i in range(len(freshIDs)):
 
 #print(freshIDs)
 
+# Part 1
+# count = 0
+# for i in ingredientIDs:
+#     for j in freshIDs:
+#         if int(i) >= j[0] and int(i) <= j[1]:
+#             #print(i)
+#             count += 1
+#             break
+# print(count)
+
+# Part2 (Doesn't work)
+# uniqueFreshIDs = {}
+# for i in range(len(freshIDs)):
+#     for j in range(freshIDs[i][0], freshIDs[i][1]+1):
+#         if uniqueFreshIDs.get(j, 0) == 0:
+#             uniqueFreshIDs[j] = 1
+
+# print(len(uniqueFreshIDs.keys()))
+
+# Real Part 2
+cleanRange = []
 count = 0
-for i in ingredientIDs:
-    for j in freshIDs:
-        if int(i) >= j[0] and int(i) <= j[1]:
-            #print(i)
-            count += 1
+for i in range(len(freshIDs)):
+    cleanLower = freshIDs[i][0]
+    cleanUpper = freshIDs[i][1]
+    for j in range(len(cleanRange)):
+        lowerRange = cleanLower >= cleanRange[j][0] and cleanLower <= cleanRange[j][1]
+        upperRange = cleanUpper >= cleanRange[j][0] and cleanUpper <= cleanRange[j][1]
+        coverRange = cleanLower < cleanRange[j][0] and cleanUpper > cleanRange[j][1]
+        if coverRange:
+            cleanRange[j] = [0, -1]
+        elif lowerRange and upperRange:
             break
+        elif lowerRange:
+            cleanLower = cleanRange[j][1]+1
+        elif upperRange:
+            cleanUpper = cleanRange[j][0]-1
+    else:
+        if cleanLower <= cleanUpper:
+            cleanRange.append([cleanLower, cleanUpper])
+    #print(cleanRange)
+for i in cleanRange:
+    count += i[1]-i[0]+1
 print(count)
+'''
+
+50-70
+44-58
+55-57
+43-59
+44-62
+1-100
+
+
+'''
