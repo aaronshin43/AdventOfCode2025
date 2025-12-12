4efdc4a317917c24b9c4253cdb68488b1b8e0fa1
aaronshin43 <aaronshin.exe@gmail.com>
2025-12-11 23:58:03 -0500

    day11 part2

--- day11.py
+++ day11.py
@@ -25,16 +25,57 @@ while True:
 # print(graph)
 
 # Part 1 DFS
-def count_paths(current, target, graph):
-    # print(current)
-    if current == target:
-        return 1
+# def count_paths(current, target, graph):
+#     # print(current)
+#     if current == target:
+#         return 1
+    
+#     total = 0
+#     for next_device in graph[current]:
+#         # print(next_device)
+#         total += count_paths(next_device, target, graph)
+#     return total
+
+# result = count_paths('you', 'out', graph)
+# print(result)
+
+
+'''
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+'''
+# Part 2
+from functools import cache
+
+@cache
+def count_paths2(current, visited_dac, visited_fft):
+    # print(f"current: {current}")
+    if current == 'dac':
+        visited_dac = True
+    if current == 'fft':
+        visited_fft = True
+    
+    if current == 'out':
+        # print(f"dac: {visited_dac}, fft: {visited_fft}")
+        if visited_dac and visited_fft:
+            return 1
+        return 0
     
     total = 0
     for next_device in graph[current]:
-        # print(next_device)
-        total += count_paths(next_device, target, graph)
+        total += count_paths2(next_device, visited_dac, visited_fft)
     return total
 
-result = count_paths('you', 'out', graph)
-print(result)
+result2 = count_paths2('svr', False, False)
+print(result2)
